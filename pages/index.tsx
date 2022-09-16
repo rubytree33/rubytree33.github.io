9cbc05026dae32f7f00f8a95585e2b48886e1559
rubytree33 <110690041+rubytree33@users.noreply.github.com>
2022-09-16 13:05:13 -0400

    wip logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,25 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
 
+const Logo = ({ className = "" }) => {
+  const size = 32
+  const strokeWidth = 1
+  return <svg width={size} height={size} className={className}>
+    <circle cx="50%" cy="50%" r="49%"
+            stroke="red" strokeWidth="2%" fill="none" />
+  </svg>
+}
+
 const Home: NextPage = () =>
   <>
     <Head>
       <title>about rubytree</title>
       <meta name="description" content="about rubytree" />
     </Head>
-    <header className="p-2 mb-4 flex flex-row items-center space-x-8
+    <header className="p-2 mb-4 flex flex-row items-baseline space-x-8 h-14
                       border-b border-rose-600 border-opacity-50 text-rose-900">
       <Link href="/" className="ml-16 p-1 pt-0  text-3xl font-extralight text-rose-600">
+        <Logo className="inline mr-1 align-middle" />
         rubytree
       </Link>
       <Link href="/">
@@ -32,6 +42,7 @@ const Home: NextPage = () =>
       </p>
     </main>
     <footer className="fixed bottom-0 p-2 mt-8">
+      <Logo className />
     </footer>
   </>
 
