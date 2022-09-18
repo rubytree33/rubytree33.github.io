d57e9110133c27e55ea1c7ff5c8ad852c472535f
rubytree33 <110690041+rubytree33@users.noreply.github.com>
2022-09-18 01:33:04 -0400

    dark mode attempt

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from 'next/document'
 class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang="en" className="text-rose-900 bg-rose-50 dark:text-rose-50 dark:bg-black">
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> */}
