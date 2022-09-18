3ff0a81b44a65a94fe9a50846966f6d8d8285da2
rubytree33 <110690041+rubytree33@users.noreply.github.com>
2022-09-18 00:54:46 -0400

    procedurally generated logo

---
+++ components/logo.tsx
@@ -0,0 +1,46 @@
+import _ from 'lodash'
+
+const Logo = ({ className = "" }) => {
+  const size = 32
+
+  const nFacets = 3
+  // units are coords 0 0 to 1 1, full turn = 1
+  const arcSign = 1  // 1 = cw, -1 = ccw
+  const startFrac = 1/2  // alignment of start of facets
+  const arcLength = 2/5  // arc length used by facets
+  const strokeWidth = 1/32
+  const pad = 1/8  // distance of vertices from circumscribing circle
+
+  const coords =
+    _.range(nFacets + 1)  // vertices of the facets
+      .map(i => i/nFacets * arcLength)  // distribute evenly
+      .concat([(1 + arcLength) / 2, 1])  // add bottom of ruby and close path
+      .map(frac => arcSign * Math.PI*2 * (startFrac + frac))  // to radians
+      .map(rad => [Math.cos(rad), Math.sin(rad)])  // to unit circle coords
+    .map(coordPair => coordPair
+      .map(xI => xI / 2)  // rescale for viewbox
+      .map(xI => xI * (1 - pad*2))  // pad
+      .map(xI => xI + 1/2)  // center in viewbox
+      .map(xI => xI * (1 - strokeWidth) + strokeWidth/2) // pad for stroke
+      .map(xI => xI * size)  // actual size
+    )
+
+  const toD = (coords: number[][]) =>
+    "M" + coords.map(([x,y])=>x+" "+y).join("L")
+
+  // just one path for now
+  const pathD = ""
+    + toD(coords)
+
+  return (
+    <svg width={size} height={size} viewBox={`0 0 ${size} ${size}`}
+        className={className + " stroke-rose-500 fill-rose-500"}>
+      <circle cx="50%" cy="50%"
+        r={size*(1-strokeWidth)/2} strokeWidth={size*strokeWidth} />
+      <path fill="transparent" stroke="white" strokeLinejoin="round"
+        d={pathD} strokeWidth={size*strokeWidth} />
+    </svg>
+  )
+}
+
+export default Logo
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,15 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
+import Logo from '../components/logo'
 
-const Logo = ({ className = "" }) => {
-  const size = 32
-  const strokeWidth = 1
-  return <svg width={size} height={size} className={className}>
-    <circle cx="50%" cy="50%" r="49%"
-            stroke="red" strokeWidth="2%" fill="none" />
-  </svg>
-}
-
-const Home: NextPage = () =>
+const Home: NextPage = () => (
   <>
     <Head>
       <title>about rubytree</title>
       <meta name="description" content="about rubytree" />
     </Head>
-    <header className="p-2 mb-4 flex flex-row items-baseline space-x-8 h-14
+    <header className="p-2 mb-4 flex flex-row items-center space-x-8 h-14
                       border-b border-rose-600 border-opacity-50 text-rose-900">
       <Link href="/" className="ml-16 p-1 pt-0  text-3xl font-extralight text-rose-600">
         <Logo className="inline mr-1 align-middle" />
@@ -40,10 +32,14 @@ const Home: NextPage = () =>
       <p>
         If you&apos;re reading this, I&apos;ve only just started making this site.
       </p>
+      <p>
+        Check out the links above for more information.
+      </p>
     </main>
     <footer className="fixed bottom-0 p-2 mt-8">
-      <Logo className />
+      <Logo />
     </footer>
   </>
+)
 
 export default Home
