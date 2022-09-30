98469a35add09dc728fae5d8a08fb9d0da83d25e
rubytree33 <110690041+rubytree33@users.noreply.github.com>
2022-09-30 17:37:14 -0400

    refactor to use layout.tsx

---
+++ components/layout.tsx
@@ -0,0 +1,33 @@
+import React, { ReactNode } from 'react'
+import Link from 'next/link'
+import Logo from './logo'
+
+interface Props {
+  children?: ReactNode
+}
+
+export default function Layout({ children }: Props) {
+  return (
+    <>
+      <header className="p-2 mb-4 flex flex-row items-center space-x-10 h-14
+                        border-b border-ruby-600 border-opacity-50 text-ruby-900 dark:text-ruby-200">
+        <Link href="/" className="ml-16 p-1 pt-0  text-3xl font-extralight text-ruby-600">
+          <Logo className="inline mr-3 align-middle" />
+          rubytree
+        </Link>
+        <Link href="https://github.com/rubytree33">
+          github
+        </Link>
+        <Link href="https://github.com/rubytree33/rubytree33.github.io">
+          source
+        </Link>
+      </header>
+      <main className="mx-auto px-8 max-w-3xl">
+        {children}
+      </main>
+      <footer className="fixed bottom-0 p-2 mt-8">
+        <Logo />
+      </footer>
+    </>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Link from 'next/link'
-import Logo from '../components/logo'
 
 const Home: NextPage = () => (
   <>
@@ -9,20 +7,7 @@ const Home: NextPage = () => (
       <title>about rubytree</title>
       <meta name="description" content="about rubytree" />
     </Head>
-    <header className="p-2 mb-4 flex flex-row items-center space-x-10 h-14
-                      border-b border-ruby-600 border-opacity-50 text-ruby-900 dark:text-ruby-200">
-      <Link href="/" className="ml-16 p-1 pt-0  text-3xl font-extralight text-ruby-600">
-        <Logo className="inline mr-3 align-middle" />
-        rubytree
-      </Link>
-      <Link href="https://github.com/rubytree33">
-        github
-      </Link>
-      <Link href="https://github.com/rubytree33/rubytree33.github.io">
-        source
-      </Link>
-    </header>
-    <main className="mx-auto px-8 max-w-3xl space-y-1">
+    <div className="space-y-1">
       <p>
         Thanks for visiting!
       </p>
@@ -32,10 +17,7 @@ const Home: NextPage = () => (
       <p>
         Check out the links above for more information.
       </p>
-    </main>
-    <footer className="fixed bottom-0 p-2 mt-8">
-      <Logo />
-    </footer>
+    </div>
   </>
 )
 
