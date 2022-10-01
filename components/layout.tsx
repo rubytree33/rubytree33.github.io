a0852f57e5ce62db5a735f70b442e1305383b687
rubytree33 <110690041+rubytree33@users.noreply.github.com>
2022-10-01 18:43:34 -0400

    adjust layout to correctly overlay header

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,10 +7,16 @@ interface Props {
 }
 
 export default function Layout({ children }: Props) {
+  const headerHeight = 'h-14 mb-4'
   return (
     <>
-      <header className="p-2 mb-4 flex flex-row items-center space-x-10 h-14
-                        border-b border-ruby-600 border-opacity-50 text-ruby-900 dark:text-ruby-200">
+      <div className={headerHeight}>
+      </div>
+      <main>
+        {children}
+      </main>
+      <header className={`${headerHeight} absolute top-0 w-full p-2 flex flex-row items-center space-x-10
+                        border-b border-ruby-600 border-opacity-50 text-ruby-900 dark:text-ruby-200`}>
         <Link href="/" className="ml-16 p-1 pt-0  text-3xl font-extralight text-ruby-600">
           <Logo className="inline mr-3 align-middle" />
           rubytree
@@ -22,9 +28,6 @@ export default function Layout({ children }: Props) {
           source
         </Link>
       </header>
-      <main>
-        {children}
-      </main>
       <footer className="fixed bottom-0 p-2 mt-8">
         <Logo />
       </footer>
