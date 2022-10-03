ffb863d6f78626cf4fb2c7f5083d9f26745bf400
rubytree33 <110690041+rubytree33@users.noreply.github.com>
2022-10-03 12:27:18 -0400

    clean up layout; height: 100%

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,30 +7,29 @@ interface Props {
 }
 
 export default function Layout({ children }: Props) {
-  const headerHeight = 'h-14 mb-4'
   return (
     <>
-      <div className={headerHeight}>
-      </div>
-      <main>
+      <header className="
+        w-full px-16 mb-4
+        h-14 py-2
+        flex flex-row items-center
+        text-ruby-900 dark:text-ruby-200">
+          <Link href="/" className="p-1 pt-0 text-3xl font-extralight text-ruby-600">
+            <Logo className="inline align-middle" />
+            <span className="sm:inline hidden ml-3">rubytree</span>
+          </Link>
+          <div className="space-x-10 sm:ml-auto ml-10">
+            <Link href="https://github.com/rubytree33">
+              github
+            </Link>
+            <Link href="https://github.com/rubytree33/rubytree33.github.io">
+              source
+            </Link>
+          </div>
+      </header>
+      <main className="h-full">
         {children}
       </main>
-      <header className={`${headerHeight} absolute top-0 w-full p-2 flex flex-row items-center space-x-10
-                        border-b border-ruby-600 border-opacity-50 text-ruby-900 dark:text-ruby-200`}>
-        <Link href="/" className="ml-16 p-1 pt-0  text-3xl font-extralight text-ruby-600">
-          <Logo className="inline mr-3 align-middle" />
-          rubytree
-        </Link>
-        <Link href="https://github.com/rubytree33">
-          github
-        </Link>
-        <Link href="https://github.com/rubytree33/rubytree33.github.io">
-          source
-        </Link>
-      </header>
-      <footer className="fixed bottom-0 p-2 mt-8">
-        <Logo />
-      </footer>
     </>
   )
 }
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,17 +5,19 @@ class MyDocument extends Document {
     return (
       <Html lang="en" className="
         text-ruby-900     bg-ruby-50
-        dark:text-ruby-50 dark:bg-ruby-950">
-        <Head>
-          <link rel="shortuct icon" href="/favicon.ico" />
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> */}
-          <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet" />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
+        dark:text-ruby-50 dark:bg-ruby-950
+        h-full
+        ">
+          <Head>
+            <link rel="shortuct icon" href="/favicon.ico" />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> */}
+            <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet" />
+          </Head>
+          <body className="h-full">
+            <Main />
+            <NextScript />
+          </body>
       </Html>
     )
   }
