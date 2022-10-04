8065b56116fb09c3f87d1abf1b6ca47eee5538ec
rubytree33 <110690041+rubytree33@users.noreply.github.com>
2022-10-04 16:06:49 -0400

    move source to src/

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ logPalette('rose')
 
 module.exports = {
   content: [
-    "./{pages,components}/**/*.{js,ts,jsx,tsx}"
+    "./src/{pages,components}/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
