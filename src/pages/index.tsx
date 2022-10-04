fee4592970b7d39e434bf1bb5b55684a433a6cfe
rubytree33 <110690041+rubytree33@users.noreply.github.com>
2022-10-04 17:52:39 -0400

    link to incomplete chess game

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Layout from '../components/layout'
+import Link from 'next/link'
 
 const Home: NextPage = () => (
   <Layout>
@@ -15,7 +16,7 @@ const Home: NextPage = () => (
         If you&apos;re reading this, I&apos;ve only just started making this site.
       </p>
       <p>
-        Check out the links above for more information.
+        I have an <Link href='/chess' className='text-red-500'>incomplete chess game</Link>.
       </p>
     </div>
   </Layout>
