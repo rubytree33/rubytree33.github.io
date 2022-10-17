8f013733ec93faa424c833442dc53981747a0e9f
rubytree33 <110690041+rubytree33@users.noreply.github.com>
2022-10-17 13:10:52 -0400

    fix weird font error

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -13,7 +13,6 @@ class MyDocument extends Document {
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             {/* <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin /> */}
             <link href="https://fonts.googleapis.com/css2?family=Titillium+Web:ital,wght@0,200;0,300;0,400;0,600;0,700;0,900;1,200;1,300;1,400;1,600;1,700&display=swap" rel="stylesheet" />
-            <link rel="preload" href="/fonts/FreeMono.woff" as="font" type="font/woff" crossOrigin="anonymous" />
           </Head>
           <body className="h-full">
             <Main />
