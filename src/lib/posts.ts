d1edf9eee9ca3413d7397d7d7e12d4027425fa82
rubytree33 <110690041+rubytree33@users.noreply.github.com>
2022-10-19 16:15:12 -0400

    blog functionality

---
+++ src/lib/posts.ts
@@ -0,0 +1,30 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import { remark } from 'remark'
+import html from 'remark-html'
+
+const postsPath = path.join(process.cwd(), 'posts')
+
+export interface Post {
+  name: string
+  title?: string
+  date?: string
+  contentHTML: string
+}
+
+export const getPostNames = (): string[] =>
+  fs.readdirSync(postsPath).map(filename => filename.replace(/\.md$/, ''))
+
+export const getPostByName = async (name: string): Promise<Post> => {
+  const parsed = matter(fs.readFileSync(path.join(postsPath, `${name}.md`)))
+  return ({
+    name,
+    contentHTML: (await remark().use(html).process(parsed.content)).toString(),
+    ...parsed.data,
+  })
+}
+
+export const getPosts = async (): Promise<Post[]> =>
+  (await Promise.all(getPostNames().map(getPostByName)))
+    .sort((a: any, b: any) => (b?.date ?? -1) - (a?.date ?? -1))
---
+++ src/pages/posts/[name].tsx
@@ -0,0 +1,27 @@
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType, NextPage } from 'next'
+import { ParsedUrlQuery } from 'querystring'
+import { Post, getPostNames, getPostByName } from '../../lib/posts'
+
+export const getStaticPaths: GetStaticPaths = async () => ({
+  paths: getPostNames().map(name => ({
+    params: { name }
+  })),
+  fallback: false,
+})
+
+interface Params extends ParsedUrlQuery { name: string }
+interface Context { params: Params }
+export const getStaticProps = async ({ params: { name } }: Context) =>
+  name ? ({
+    props: { post: await getPostByName(name) }
+  }) : {}
+
+interface Props { post: Post }
+const Page = ({ post }: Props) => <>
+  <h1>{post.name}</h1>
+  <p>{post.date}</p>
+  <br />
+  <main dangerouslySetInnerHTML={{ __html: post.contentHTML }} />
+</>
+
+export default Page
